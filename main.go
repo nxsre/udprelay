c23a165f44783f00b70a9675d892371ff50db985
Katie Wolfe <wolfe@katie.host>
2020-08-18 05:02:09 -0400

    v0.0.0

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type Peer struct {
 func main() {
 	log.SetFlags(0)
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s [OPTION...] port\n\noptions:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "udprelay %s\nusage: %s [OPTION...] port\n\noptions:\n", Version, os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
